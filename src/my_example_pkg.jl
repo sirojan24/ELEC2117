b15d1552e0f8c02cf95cb6533c91e6b182b59c91
sirojan09 <sirojan09@gmail.com>
2024-09-11 22:35:49 +1000

    Initial Commit

--- src/my_example_pkg.jl
+++ src/my_example_pkg.jl
@@ -1,5 +1,10 @@
 module my_example_pkg
 
 # Write your package code here.
+using DifferentialEquations # this pkg is not used in this simple example
+using Plots
+export sum_values
+include("my_func.jl")
 
 end
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,7 @@ using my_example_pkg
 using Test
 
 @testset "my_example_pkg.jl" begin
-    # Write your tests here.
+    @test sum_values(2,3) == 5
+    @test sum_values(-1,1) == 0
+    @test sum_values(0,0) == 0
 end
